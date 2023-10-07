ebf175c4cf3cd0ec34c4d6a08ad8d336cf644f11
hugo-esquer <hugo.esquer@laplateforme.io>
2023-10-07 14:44:04 +0200

    changement utilisateur Job09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -4,14 +4,33 @@
 csv_file="Shell_Userlist.csv"
 
 # Parcours du fichier CSV
-while IFS=',' read -r id prenom nom mdp role;
-do 
-# Creation de l'utilisateur
-	if [ "$id" != "Id" ];then
-		sudo useradd --badname -p `mkpasswd "$mdp"` -d /home/"$prenom $nom" -m -g users -s /bin/bash "$prenom $nom"
-	fi
-# Elévation des privilèges administrateur
-	if [ "$role" == "Admin" ];then
-		sudo useradd "$prenom $nom" sudo
+while IFS=',' read -r id prenom nom mdp role; do 
+
+	# Saut de la première ligne
+	if [ "$id" != "Id" ]; then
+
+		# Vérification de l existance de l utilisateur
+		if id "$prenom" &>/dev/null; then
+
+			#modification de l'utilisateur
+			sudo usermod -m -d "/home/$prenom" -c "$prenom $nom" -p `mkpasswd "$mdp"` "$prenom"
+
+			# Elévation ou suppression des privilèges administrateur
+			if [ "$role" = "Admin" ]; then
+				sudo usermod -aG sudo "$prenom"
+			else
+				sudo deluser "$prenom" sudo &>/dev/null
+			fi
+
+		else
+
+			# Création de l'utilisateur
+			sudo useradd --badname -m "$prenom" -d "/home/$prenom" -G users -c "$prenom $nom" -p `mkpasswd "$mdp"`
+
+			# Elévation des privilèges administrateur
+			if [ "$role" == "Admin" ]; then
+				sudo useradd -aG "$prenom" sudo
+			fi
+		fi
 	fi
 done < "$csv_file"
